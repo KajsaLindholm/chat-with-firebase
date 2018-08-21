37bea7fe94473674c5241b4b3d67527ba8034b3f
Kajsa Lindholm <kajsamlindholm@gmail.com>
2018-08-21 10:44:27 +0200

    nothing exciting

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import firebase from './firebase';
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hej</h1>
+        <button>please sign in</button>
+      </div>
+    );
+  }
+}
+
+export default App;
